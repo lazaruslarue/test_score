409315221fc240ddc08febffd92d47fe332a9f64
lazaruslarue <lazaruslarue@gmail.com>
2016-08-31 17:31:05 -0600

    use Test

--- src/main.js
+++ src/main.js
@@ -5,62 +5,60 @@ import {makeRouterDriver} from 'cyclic-router'
 import {createHistory} from 'history'
 import storageDriver from '@cycle/storage'
 
-
-
-//view
 import {div, p, input} from '@cycle/dom'
+import Test from './Test'
 
-function intent(DOM, storage) {
-  // kepress events from input element
-  const exitvalue$ = DOM.select('input')
-    .events('keyup')
-    .map(function(ev) {
-      return {
-        key: 'inputText',
-        value: ev.target.value
-      };
-    })
 
-  return {
-    storage: storage.local.getItem('inputText')
-      .startWith(''),
-    DOM: exitvalue$,
-  }
-}
-
-function model({request$, storage}) {
-  let stored$ = storage.startWith('')
-  return stored$
-}
-
-function view(state$) {
-  return state$.map((text) => {
-    console.log('view', text);
-     return div([input({props: {
-       type: 'text',
-       value: text
-     }}), p(text)])
-    })
-}
-
-function main(sources) {
-  // returns:  keypress input$ & storage$
-  let action$ = intent(sources.DOM, sources.storage)
-  // write dom elements from storage
-  // store inputs locally
-  let state$ = model(action$, sources.storage)
-  // vDom$
-  let view$ = view(state$)
-
-  return {
-    DOM: view$,
-    storage: action$.DOM,
-  }
-}
-
+const main = Test;
 const drivers = {
   DOM: makeDOMDriver('#root'),
   storage: storageDriver,
 };
 
 run(main, drivers);
+//
+// function intent(DOM, storage) {
+//   // kepress events from input element
+//   const exitvalue$ = DOM.select('input')
+//     .events('keyup')
+//     .map(function(ev) {
+//       return {
+//         key: 'inputText',
+//         value: ev.target.value
+//       };
+//     })
+//
+//   return {
+//     storage: storage.local.getItem('inputText')
+//       .startWith(''),
+//     DOM: exitvalue$,
+//   }
+// }
+//
+// function model({request$, storage}) {
+//   const stored$ = storage.startWith('')
+//   return stored$
+// }
+//
+// function view(state$) {
+//   return state$.map((text) => {
+//      return div([input('.input',{props: {
+//        type: 'text',
+//        value: text
+//      }}), p(text)])
+//     })
+// }
+//
+// function butts(sources) {
+//   // returns:  keypress from DOM & storage$
+//   let action$ = intent(sources.DOM, sources.storage)
+//   // store inputs locally
+//   let state$ = model(action$, sources.storage)
+//   // vDom$
+//   let view$ = view(state$)
+//
+//   return {
+//     DOM: view$,
+//     storage: action$.DOM,
+//   }
+// }
