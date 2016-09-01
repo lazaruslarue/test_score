a3773941fa02507d21e7c909db227a56201dee54
lazaruslarue <lazaruslarue@gmail.com>
2016-08-31 23:04:27 -0600

    hijack some de/serialize code

---
+++ src/Test/storage-sink.js
@@ -0,0 +1,25 @@
+// Turn the data object that contains
+// the todos into a string for localStorage.
+// export default function serialize(scores$) {
+//   return scores$.map(test => JSON.stringify(
+//     {
+//       list: test.scores.map(scoreData =>
+//         ({
+//           name: scoreData.name,
+//           score: scoreData.score,
+//         })
+//       )
+//     }
+//   ));
+// };
+
+
+// Turn the data object that contains
+export default function serialize(storage$) {
+
+  return storage$.map(test => {
+    console.log('storage serialize', test)
+
+    return JSON.stringify(test)
+  })
+};
